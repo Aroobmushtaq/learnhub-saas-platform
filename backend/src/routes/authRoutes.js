9463f346cdc4c2b1eefa8e0ab64be81dc7b9fe4b
AroobMushtaq <aroobmushtaq786@gmail.com>
2025-09-09 21:22:40 +0500

    protected route added

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,7 +1,10 @@
 import exspress from 'express';
 import { registerUser, loginUser } from '../controllers/authController.js';
+import { protect } from '../middleware/authMiddleware.js';
 const router = exspress.Router();
-
+router.get('/profile',protect,(req,res)=>{
+    res.json({message:`Welcome ${req.user.name}`,user:req.user});
+})
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
